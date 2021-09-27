55727bc17749855a0c2bae4f3983e9af8b741947
norraynor <alechanderr@gmail.com>
2021-09-27 19:27:34 +0200

    setup project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      --display cards here--
+      --clicking on card shuffles them randomly and remembers card clicked--
+      --clicking the same card twice results in game over and reseting the score and saving best score if its bigger than previous one--
+      
+    </div>
+  );
+}
+
+export default App;
