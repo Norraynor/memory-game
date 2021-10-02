f3ee14f5c86cfdfafd7f9ff96503bca38364a4e2
norraynor <alechanderr@gmail.com>
2021-10-02 21:03:25 +0200

    finished memory pokemon game

--- src/App.js
+++ src/App.js
@@ -1,11 +1,97 @@
 import React, {useState} from 'react';
 import './App.css';
 import Card from "./components/Card.js"
+import Abra from "./images/Abra.png"
+import Blastoise from "./images/Blastoise.png"
+import Charizard from "./images/Charizard.png"
+import Dratini from "./images/Dratini.png"
+import Eevee from "./images/Eevee.png"
+import Gastly from "./images/Gastly.png"
+import Kakuna from "./images/Kakuna.png"
+import Machamp from "./images/Machamp.png"
+import Pidgey from "./images/Pidgey.png"
+import Pikachu from "./images/Pikachu.png"
+
+
 
 function App() {
-  const [score,setScore] = useState({});
-  const [bestScore,setBestScore] = useState({});
+  const cardsArray = [
+    {
+      name: "Abra",
+      img: Abra
+    },
+    {
+      name: "Blastoise",
+      img: Blastoise
+    },
+    {
+      name: "Charizard",
+      img: Charizard
+    },
+    {
+      name: "Dratini",
+      img: Dratini
+    },
+    {
+      name: "Eevee",
+      img: Eevee
+    },
+    {
+      name: "Gastly",
+      img: Gastly
+    },
+    {
+      name: "Kakuna",
+      img: Kakuna
+    },
+    {
+      name: "Machamp",
+      img: Machamp
+    },
+    {
+      name: "Pidgey",
+      img: Pidgey
+    },
+    {
+      name: "Pikachu",
+      img: Pikachu
+    }
+  ];
+  const [score,setScore] = useState(0);
+  const [bestScore,setBestScore] = useState(0);
+  const [clickedArray, setClickedArray] = useState([]);
 
+  function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
+  function setClicked(name){
+    if(clickedArray.includes(name)){
+      if(score>bestScore){
+        setBestScore(score);
+      }
+      setScore(0);
+      setClickedArray([]);
+
+    }else{
+      setClickedArray([...clickedArray,name]);
+      setScore(score+1);
+    }
+  }
   //add cards as objects with names and img
   return (
     <div className="App">
@@ -13,22 +99,16 @@ function App() {
         <div id="title">POKEMON MEMORY GAME</div>
         <div id="score">
           <div>score: {score} </div>
-          <div>best score: {bestScore} </div>
+          <div>best score: {bestScore}  </div>
         </div>
       </div>
-      --display cards here--
-      --clicking on card shuffles them randomly and remembers card clicked--
-      --clicking the same card twice results in game over and reseting the score and saving best score if its bigger than previous one--
+      <div id="description">RULES: Click once per card to get a point. Each click reshuffles the cards.</div>
       <div id="cards">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-
+        {
+          shuffle(cardsArray).map(element => {
+            return (<Card name={element.name} img={element.img} clickHandler={setClicked}/>)
+          })
+        }
       </div>
 
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,13 @@ import React from 'react';
 import '../styles/Card.css';
 
 function Card(props){
+    function handleClick(){
+        props.clickHandler(props.name);
+    }
     return(
-        <div className="card">
-            <h1>card here</h1>
+        <div className="card" onClick={handleClick}>
             <img src={props.img} alt={props.name}/>
-            <h3>{props.name}</h3>
+            <h3 id="title">{props.name}</h3>
         </div>
     );
 }
